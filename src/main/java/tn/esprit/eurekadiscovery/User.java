02baa0c710fcaadfb40ebfb1636f36a5aaed32ac
ChahirSghaier <chahir.sghaier@esprit.tn>
2023-10-19 22:56:42 +0100

    class User 2

--- src/main/java/tn/esprit/eurekadiscovery/User.java
+++ src/main/java/tn/esprit/eurekadiscovery/User.java
@@ -4,6 +4,8 @@ public class User {
     private int id;
     private String userName;
     private String email;
+
+    public final static String WELCOME = "welcome";
     public User(){}
     public User(String email,String userName){
         this.userName=email;
